8772ce1c90cbc859a0e9e533adc5f40af2203f28
Wim Vandevenne <info@wiven.be>
2015-05-23 18:39:41 +0200

    Created new controller for the user side

---
+++ application/controllers/User.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+    public function index() {
+        $data_header = array(
+            'page_title' => ' - Gebruikersinfo'
+            //'language_test' => lang('hello')
+        );
+
+        $this->load->view('/common/header', $data_header);
+        $this->load->view('/common/top_menu_clients');
+        $this->load->view('user');
+        $this->load->view('/common/footer_clients');
+    }
+
+    public function profile() {
+        $data_header = array(
+            'page_title' => ' - Profile'
+        );
+
+        $this->load->view('/common/header', $data_header);
+        $this->load->view('/common/top_menu_clients');
+        $this->load->view('user_profile');
+        $this->load->view('/common/footer_clients');
+    }
+}
