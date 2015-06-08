218a039ca30a66673bd21dce123cca7874922af0
Wim Vandevenne <info@wiven.be>
2015-06-08 18:42:46 +0200

    Made the modal for general conditions larger, this to prevent endless scrolling

--- application/views/register.php
+++ application/views/register.php
@@ -118,7 +118,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <!-- Modal -->
 <div class="modal fade" id="generalConditionsModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
