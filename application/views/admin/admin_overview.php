529ad09d41f266191e6b4ba15e9355a645e5cf5d
Baggeraar <michiel.delrue@outlook.com>
2015-07-13 21:42:52 +0200

    overvieuw commit

--- application/views/admin/admin_overview.php
+++ application/views/admin/admin_overview.php
@@ -23,5 +23,29 @@
     </div>
 </div>
 
+<div class="col-lg-12">
+    <div class="panel panel-success"> 
+        <div class="panel-heading">Partners</div>
+        <div class="row">
+            <div class="col-lg-2 col-sm-12" style="margin-left: 20px;" >
+                <div class="bs-callout bs-callout-default" style="text-align: center;">
+                    <h4>Onze partners:</h4>
+                    <p style="text-align: left;">
+                        </br>
+                        Restaurant At Home werkt samen met enkele grote bedrijven waardoor U ook van onze kortingen kan genieten!
 
-
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-12" style="max-height:200px;">
+                <div class="thumbnail">
+                    <div class="caption">
+                        <h3>Bestel Restaurant at Home verpakking</h3>
+                        <p><a href="#" class="btn btn-primary" role="button">Bestel</a></p>
+                    </div>
+                    <img src="http://www.mengelmoes.net/wp-content/uploads/2014/07/Moonen-Packaging-logo.jpg" alt="Moonen Packaging">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
