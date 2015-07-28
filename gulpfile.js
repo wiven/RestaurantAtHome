76187ea92e820278f0c85b276e228c25d78725a0
Frederik De Roover <derooverfrederik@gmail.com>
2015-07-28 10:01:19 +0200

    Added image optimizer to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 
 gulp.task('minify', function () {
 	gulp.src('public/css/*.css')
@@ -11,6 +13,16 @@ gulp.task('minify', function () {
 			suffix: '.min'
 		}))
 		.pipe(gulp.dest('public/css/min'));
+
+	return gulp.src('public/img/optim/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{
+				removeViewBox: false
+			}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('public/img/'));
 });
 
 gulp.task('default', ['minify'], function () {
